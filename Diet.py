342bf939c5cb1384cfee0d8501a7882a7f4dbfe0
Shaik Nazeer pasha <sknazeerpasha995@gmail.com>
2025-07-18 12:39:18 +0530

    Diet.py

--- Diet.py
+++ Diet.py
@@ -15,7 +15,7 @@ def set_background_local(image_file):
         background-attachment: fixed;
     }}
 
-    @media only screen and (max-width: 768px) {{
+    @media only screen and (max-width: 120px) {{
         .stApp {{
             background-size: contain;
             background-position: center top;
